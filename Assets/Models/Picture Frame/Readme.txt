eafc0509ae0e3134469e3fef65275441c250e2c6
Adam Ulidowski <adam.ulidowski@wp.pl>
2018-03-22 15:26:51 +0100

    Pierwszy commit
    Commit startowy testowy, aplikacja odala się na androidzie

---
+++ Assets/Models/Picture Frame/Readme.txt
@@ -0,0 +1,9 @@
+Created by Logan Willmert
+
+I would like to know how my objects are used, and request that you would please send me a render of what you have used my object in. Thank you for buying my model.
+
+All objects and textures were created by me unless credit is given otherwise (see below).
+
+-------------------------------------------------------------------------------------------------------------
+
+Picture dowloaded from www.wallpapers.com
---
+++ Assets/TextureLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using System.Threading;
+
+public class TextureLoader : MonoBehaviour {
+
+    public string url = "http://dami94.2ap.pl/hipertrofia.jpg";
+
+    // Use this for initialization
+    IEnumerator Start () {
+
+        // Start a download of the given URL
+        WWW www = new WWW(url);
+
+        // Wait for download to complete
+        yield return www;
+
+        // assign texture
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = www.texture;
+
+     
+    }
+
+    private IEnumerator waitForDownload() {
+        yield return new WaitForSeconds(0.1f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
